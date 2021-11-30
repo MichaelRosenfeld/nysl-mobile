4ce511f79990724bb827b1150723b74322f8117a
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-11-30 14:25:13 +0100

    Wrap Home around Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/home/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
-   <Header />
+   <Home />
   );
 }
 
