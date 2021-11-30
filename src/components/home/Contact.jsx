eb013156ac325501f4969e51fb1a169452fd37a7
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-11-30 14:58:31 +0100

    Add ContactInfo

---
+++ src/components/home/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+    return (
+        <>
+            <p>Please email us at <a href="mailto:nysl@chisoccer.org">nysl@chisoccer.org</a></p>
+            <p id="contact-bottom">We will reply to your email as soon as we can.</p>
+        </>
+    );
+
+}
+
+export default Contact;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Events from "./Events";
+import Contact from "./Contact";
 
 function Home() {
     return (
         <>
             <Header />
             <Events />
+            <Contact />
         </>
     );
 }
