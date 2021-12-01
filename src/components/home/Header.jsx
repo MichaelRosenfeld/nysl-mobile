325d2c4289110609004972b7f8351b8431133c4c
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-12-01 08:52:43 +0100

    Refactor jsx

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,10 +1,14 @@
 const title = 'Northside Youth Soccer League';
 
+const header = <div className="header-container">
+                    <h1 className="title">{title}</h1>
+                </div>;
+
 function Header() {
     return (
-        <div className="header-container">
-            <h1 className="title">{title}</h1>
-        </div>
+        <>
+        {header}
+        </>
     );
 }
 
