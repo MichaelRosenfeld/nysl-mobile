243c76e437f482c2b534ea471fa1b09d45584f61
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-12-01 13:06:53 +0100

    Add Styling to home

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,11 +1,12 @@
 const staticEvents= (
     <div className="Events">
+        <h3 className="EventsTitle">Upcoming</h3>
         <p className="Date">August 4</p>
         <p className="Description">NYSL Fundraiser</p>
         <p className="Date">August 16</p>
         <p className="Description">Season Kick-off: Meet the Teams</p>
         <p className="Date">September 1</p>
-        <p className="Description">First Game of the Season (Check Game Schedule for details)</p>;
+        <p className="Description">First Game of the Season (Check Game Schedule for details)</p>
     </div>
 );
 
